64984a54e4152308783eda9378905ce3fe35decd
vanessalb08 <vanessalimab08@gmail.com>
2021-07-01 00:36:27 -0300

    "REsolução desafio 63"

--- desafio063.py
+++ desafio063.py
@@ -1,4 +1,12 @@
-num = int(input('Digite um número: '))
+num = int(input('Quantos elementos quer ver? '))
+termo = 0
+soma = 1
+cont = 1
 fib = 0
-while fib != num:
-    num = num -(num - 1)
+while cont <= num:
+    fib = termo + soma
+    print(termo, end='➔ ')
+    soma = termo
+    termo = fib
+    cont += 1
+print('Fim')
